621fa541e610479a9c9de0289754f69f66f40bec
LONGXUANX <68266379+LONGXUANX@users.noreply.github.com>
2025-03-26 02:27:34 +0800

    Update SeletionForComparison.vue

--- src/components/sections/SeletionForComparison.vue
+++ src/components/sections/SeletionForComparison.vue
@@ -78,16 +78,129 @@ export default {
 </script>
 
 <template>
-  <!-- 保持原有的template结构不变 -->
   <div>
     <el-divider />
+
     <el-row justify="center">
       <h1 class="section-title">Comparison</h1>
     </el-row>
-    <!-- ...原有template内容... -->
+
+    <el-row justify="center">
+      <el-col >
+        <el-row justify="center" :gutter="20">
+
+          <el-col :xs="12" :sm="10" :md="8" :lg="6" :xl="6" >
+
+            <div class="demo-image">
+              <div class="block">
+                <el-skeleton
+                style="width: 100%"
+                :loading="comparison1Loading"
+                animated
+                :throttle="1000"
+                >
+                  <template #template>
+                    <el-skeleton-item variant="image" style="width: 100%; height: 100%" />
+                  </template>
+                  <template #default>
+                    <el-image :src="comparison1ImageRootPath" style="width: 100%; height: 100%" fit="scale-down"/>
+                  </template>
+                </el-skeleton>
+                <span class="demonstration">input: {{ comparison1ImageRootPath }}</span>
+              </div>
+            </div>
+
+            <el-row justify="center">
+                <el-select
+                  class="select"
+                  v-model="value1"
+                  placeholder="Select"
+                  size="large"
+                  @change="handleChange1"
+                >
+                  <el-option
+                    v-for="item in options"
+                    :key="item"
+                    :label="item"
+                    :value="item"/>
+                </el-select>
+            </el-row>
+
+          </el-col>
+
+          <el-col :xs="12" :sm="10" :md="8" :lg="6" :xl="6" >
+            
+            <div class="demo-image">
+              <div class="block">
+                <el-skeleton
+                style="width: 100%"
+                :loading="comparison2Loading"
+                animated
+                :throttle="1000"
+                >
+                  <template #template>
+                    <el-skeleton-item variant="image" style="width: 100%; height: 100%" />
+                  </template>
+                  <template #default>
+                    <el-image :src="comparison2ImageRootPath" style="width: 100%; height: 100%" fit="scale-down"/>
+                  </template>
+                </el-skeleton>
+                <span class="demonstration">input: {{ comparison2ImageRootPath }}</span>
+              </div>
+            </div>
+
+            <el-row justify="center">
+              <el-select
+                class="select"
+                v-model="value2"
+                placeholder="Select"
+                size="large"
+                @change="handleChange2"
+              >
+                <el-option
+                  v-for="item in options"
+                  :key="item"
+                  :label="item"
+                  :value="item"/>
+              </el-select>
+            </el-row>
+          </el-col>
+
+        </el-row>
+      </el-col>
+    </el-row>
+
   </div>
 </template>
 
 <style scoped>
-/* 保持原有样式不变 */
+
+.select {
+  padding-top: 10px;
+  width: 200px;
+}
+
+/* 路径文字居中 */
+.demo-image .block {
+  padding: 20px 0 0 0;
+  text-align: center;
+  border-right: solid 1px var(--el-border-color);
+  display: inline-block;
+  width: 100%;
+  box-sizing: border-box;
+  vertical-align: top;
+}
+
+.demo-image .block:last-child {
+  border-right: none;
+}
+
+/* 路径文字颜色 */
+.demo-image .demonstration {
+  padding-top: 10px;
+  display: block;
+  color: var(--el-text-color-secondary);
+  word-wrap: break-word;
+}
+
 </style>
